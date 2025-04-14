47d79ffd8581c65b5a5cc7f535accdbf11bae31c
Ruben Gomez <ruben@khromalabs.org>
2025-04-14 12:04:52 +0200

    Add opacity property to SplashWindow configuration

--- polaris/windows/SplashWindow.js
+++ polaris/windows/SplashWindow.js
@@ -14,6 +14,7 @@ class SplashWindow extends BaseWindow {
             skipTaskbar: true,
             backgroundColor: '#000000FF',
             alwaysOnTop: true,
+            opacity: 1,
             webPreferences: {
                 nodeIntegration: true,
                 contextIsolation: false
