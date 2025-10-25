d2f8a483b7761a7a800f7e1714ce457020c91335
Ruben Gomez <ruben@khromalabs.org>
2025-10-25 17:24:50 +0200

    Re-enable reasoning in litellm

--- ainara/framework/llm/litellm.py
+++ ainara/framework/llm/litellm.py
@@ -326,9 +326,9 @@ class LiteLLM(LLMBackend):
                     elif reasoning_level > 0.33:
                         reasoning_effort_str = "medium"
 
-                    # completion_kwargs["reasoning_effort"] = (
-                    #     reasoning_effort_str
-                    # )
+                    completion_kwargs["reasoning_effort"] = (
+                        reasoning_effort_str
+                    )
                     self.logger.info(
                         f"[DISABLED] Requesting '{reasoning_effort_str}' reasoning for"
                         f" model {provider['model']}"
